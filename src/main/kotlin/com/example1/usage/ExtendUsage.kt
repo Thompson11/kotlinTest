0663d58b356f5a0158df7d7179573ccdd832c29f
Thompson11 <andyLUGEYU195268>
2018-04-10 00:06:34 +0800

    kotlin2

---
+++ src/main/kotlin/com/example1/usage/ExtendUsage.kt
@@ -0,0 +1,8 @@
+package com.example1.usage
+
+import com.example.kotlin.lastIndex
+
+fun usage() {
+    val list = arrayOf(1, 2, 3, 4)
+    println(list.lastIndex)
+}
---
+++ src/test/kotlin/com/example/kotlin/demoTest/ExtendUsageTest.kt
@@ -0,0 +1,15 @@
+package com.example.kotlin.demoTest
+
+import com.example1.usage.usage
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+
+@RunWith(JUnit4::class)
+class ExtendUsageTest {
+
+    @Test
+    fun usageTest() {
+        usage()
+    }
+}
